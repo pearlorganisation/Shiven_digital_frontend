2634f3d8b2d26f011d0c00aed07c2bef3a6a6910
SakshiSainiPearl <sakshisaini@pearlorganisation.com>
2025-09-26 15:21:28 +0530

    dashboards

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
       ),
       children: [
         {
-          path: "/",
+          path: "/dashboard",
           element: <Dashboard />,
         },
         {
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,21 @@
-"use client";
-
+import { useSelector } from "react-redux";
+import AdminDashboard from "./AdminDashboard";
+import AgencyDashboard from "./AgencyDashboard";
+import UserDashboard from "./UserDashboard";
+import NotFoundPage from "@/component/Fallback/NotFound";
+import AgencyStaffDashboard from "./AgencyStaffDashboard";
 
 const Dashboard = () => {
+  const role = useSelector((state: any) => state.auth.user?.role);
+  console.log("check role in dashboard",role)
 
+  if (role === "admin") return <AdminDashboard />;
+  if (role === "agency") return <AgencyDashboard />;
+  if (role === "user") return <UserDashboard />;
+  if (role === "agencyStaff") return <AgencyStaffDashboard/>
+  if (role === "adminStaff") return <AgencyStaffDashboard/>
 
-  return <div className="mt-2">Dashboard</div>;
+  return <NotFoundPage />;
 };
 
 export default Dashboard;
